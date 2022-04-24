2e437e7089e4b5fca34b9bea307385a5ba844baf
gideonoludeyi <gooludeyi@gmail.com>
2022-04-23 23:00:43 -0400

    include template files

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from itertools import groupby
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from unscramble import unscramble
+
+app = FastAPI()
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+templates = Jinja2Templates(directory='templates')
+
+
+@app.get('/', response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse('index.html.jinja2', {'request': request})
+
+
+@app.get('/unscramble', response_class=HTMLResponse)
+def result(request: Request, chars: str):
+    unscrambled = unscramble(chars)
+    result = groupby(unscrambled, key=len)
+    return templates.TemplateResponse('unscramble.html.jinja2', {'request': request, 'chars': chars, 'result': result})
--- unscramble/__init__.py
+++ unscramble/__init__.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+import nltk  # type: ignore
+from nltk.downloader import download  # type: ignore
 
-app = FastAPI()
+download('words', download_dir='./.nltk_data')
+nltk.data.path.append('./.nltk_data')
+
+wordlist = nltk.corpus.words.words()
 
 
-@app.get('/')
-def index() -> str:
-    return 'Hello world'
+def unscramble(chars: str) -> list[str]:
+    chars = chars.replace(' ', '').lower()
+
+    puzzle_letters = nltk.FreqDist(chars)
+    required_letters = list(chars)
+
+    results = set()
+    for word in wordlist:
+        if 2 < len(word) <= len(chars) and nltk.FreqDist(word) <= puzzle_letters:
+            for letter in required_letters:
+                if letter in word:
+                    results.add(word)
+
+    return sorted(results, key=len, reverse=True)
