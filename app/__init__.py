edd52a5439504d1f821fc423e960e702517cca20
gideonoludeyi <gooludeyi@gmail.com>
2022-04-25 03:19:05 -0400

    annotate fastapi routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from itertools import groupby
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -14,12 +14,12 @@ templates = Jinja2Templates(directory='templates')
 
 
 @app.get('/', response_class=HTMLResponse)
-def index(request: Request):
+def index(request: Request) -> Response:
     return templates.TemplateResponse('index.html.jinja2', {'request': request})
 
 
 @app.get('/unscramble', response_class=HTMLResponse)
-def result(request: Request, chars: str):
+def result(request: Request, chars: str) -> Response:
     unscrambled = sorted(unscramble(chars), key=len, reverse=True)
     num_of_words = len(unscrambled)
     groups = groupby(unscrambled, key=len)
