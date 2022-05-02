fa00358cb928e0f757183d4bbc6a2a7f36d72f5b
gideonoludeyi <gooludeyi@gmail.com>
2022-05-02 17:13:10 -0400

    replace Spacy vocabulary with NLTK

--- unscramble/__init__.py
+++ unscramble/__init__.py
@@ -1,8 +1,11 @@
+import os
 from .base import Vocabulary, WordFilter
 from .compose_filter import ComposeFilter
 from .length_range_filter import LengthRangeFilter
 from .selection_filter import SelectionFilter
-from .spacy_vocab import SpacyVocab
+from .nltk_vocab import NLTKVocabulary
+
+NLTK_DATA = os.getenv('NLTK_DATA', 'local_data')
 
 
 def unscramble(chars: str) -> set[str]:
@@ -11,7 +14,7 @@ def unscramble(chars: str) -> set[str]:
         SelectionFilter(chars),
     ])
 
-    vocab: Vocabulary = SpacyVocab()
+    vocab: Vocabulary = NLTKVocabulary(path=NLTK_DATA)
 
     return apply_word_filter(vocab, word_filter)
 
